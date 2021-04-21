8a7fddae5fd756b8ca8c9963766fa82b33d47fd2
shboulz <shboul89@gmail.com>
2021-04-21 12:01:50 -0400

    Initial commit

--- FlusherFinder.Data/Note.cs
+++ FlusherFinder.Data/Note.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,26 @@ namespace FlusherFinder.Data
 {
     public class Note
     {
+        [Key]
+        public int NoteId { get; set; }
+
+        [ForeignKey(nameof(Rating))]
+        public int RatingId { get; set; }
+
+        public virtual Rating Rating { get; set; }
+
+        [Required]
+        public Guid SubId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
