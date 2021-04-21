7ec69f58a7799c5db5c6b84e636d4a5469637880
amoyer-blip <75403219+amoyer-blip@users.noreply.github.com>
2021-04-21 13:26:05 -0400

    Initial Commit

--- FlusherFinder.Data/Location.cs
+++ FlusherFinder.Data/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,23 @@ namespace FlusherFinder.Data
 {
     public class Location
     {
+        [Key]
+        public int LocationId { get; set; }
+
+        [Required]
+        public string LocationName { get; set; }
+
+        [Required]
+        public string LocationAddress { get; set; }
+
+        public virtual List<Rating> Ratings { get; set; } = new List<Rating>();
+
+        public double Rating { get; set; }
+
+        public double IsRecommended { get; set; }
+
+        public double IsFamilyFriendly { get; set; }
+
+        public double IsTwentyFourHour { get; set; }
     }
 }
