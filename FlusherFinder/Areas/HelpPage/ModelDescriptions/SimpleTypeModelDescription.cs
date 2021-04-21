a646600e52c394d8037d049a43326d03154d43b8
amoyer-blip <75403219+amoyer-blip@users.noreply.github.com>
2021-04-21 10:06:04 -0400

    Add project files.

---
+++ FlusherFinder/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FlusherFinder.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ FlusherFinder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FlusherFinder.WebApiApplication" Language="C#" %>
