574b0312d280eab3e9dcb61f9164278ea9579993
Rochelle Deulley <rochelle.deulley@gmail.com>
2021-04-21 12:22:32 -0500

    initial commit

--- FlusherFinder.Data/Rating.cs
+++ FlusherFinder.Data/Rating.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace FlusherFinder.Data
 {
     public class Rating
     {
+        [Key]
+        public int RatingId { get; set; }
+
+        [ForeignKey(nameof(Location))]
+        
+        public int LocationId { get; set; }
+        public virtual Location Location { get; set; }
+       
+        [Required]
+        [Range(0,10)]
+        public double CleanlinessRating { get; set; }
+
+        [Required, Range(0,10)]
+        public double AccessibilityRating { get; set; }
+
+        [Required, Range(0,10)]
+        public double AmenitiesRating { get; set; }
     }
 }
---
+++ FlusherFinder.Models/RatingCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlusherFinder.Models
+{
+    public class RatingCreate
+    {
+        [Required]
+        public double CleanlinessRating { get; set; }
+
+        [Required]
+        public double AccessibilityRating { get; set; }
+
+        [Required]
+        public double AmenitiesRating { get; set; }
+    }
+}
